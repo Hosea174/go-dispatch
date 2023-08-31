d749951b85d034d2d475b811ec9dbfead214da08
Hosea174 <hossaena2018@gmail.com>
2023-09-01 02:23:13 +0300

    created button and header components

---
+++ src/scripts/main.js
@@ -0,0 +1,21 @@
+const navToggler = document.querySelector(".nav-toggler");
+const navMenu = document.querySelector(".nav-links");
+const navLinks = document.querySelectorAll(".navbar a");
+
+allEventListners();
+
+function allEventListners() {
+  navToggler.addEventListener("click", togglerClick);
+  navLinks.forEach((elem) => elem.addEventListener("click", navLinkClick));
+}
+
+function togglerClick() {
+  navToggler.classList.toggle("toggler-open");
+  navMenu.classList.toggle("open");
+}
+
+function navLinkClick() {
+  if (navMenu.classList.contains("open")) {
+    navToggler.click();
+  }
+}
