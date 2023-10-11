85ee2069a0241afc5402ea962327994c8e45643d
Hosea174 <hossaena2018@gmail.com>
2023-10-11 12:55:50 +0300

    added sitemap and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   plugins: ["@astro/plugin-sass"],
-
+  site: "https://www.godispatch.io/",
+  integrations: [sitemap(), robotsTxt()]
 });
